11d8f916e4b55d684715ba80e3b1da95528f3a56
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-09 11:48:15 -0400

    ...

--- perftest/run.sh
+++ perftest/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -e REGISTRY_URL=http://127.0.0.1:9090/apis/registry/v1 --network="host" -it apicurio/apicurio-perftest-load:latest
+docker run -e REGISTRY_URL=http://127.0.0.1:9090/apis/registry/v1 -e TEST_RAMP_TIME=5 --network="host" -it apicurio/apicurio-perftest-load:latest
