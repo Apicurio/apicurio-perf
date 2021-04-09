c26070f5d55c0faf50f1e1008b150033c5aa5ff3
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-09 11:17:46 -0400

    ...

--- perftest/docker/run.sh
+++ perftest/docker/run.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+export PATH
+export GATLING_HOME
+export REGISTRY_URL
+export TEST_USERS
+export TEST_RAMP_TIME
+export TEST_REPORT_RESULTS
+export TEST_AGGREGATOR_HOST
+
+echo "-------------------------------------------------------------------------"
+echo "Running Apicurio Registry Performance Test (load generator)"
+echo "---"
+echo "PATH: $PATH"
+echo "GATLING_HOME: $GATLING_HOME"
+echo "REGISTRY_URL: $REGISTRY_URL"
+echo "TEST_USERS: $TEST_USERS"
+echo "TEST_RAMP_TIME: $TEST_RAMP_TIME"
+echo "TEST_REPORT_RESULTS: $TEST_REPORT_RESULTS"
+echo "TEST_AGGREGATOR_HOST: $TEST_AGGREGATOR_HOST"
+echo "-------------------------------------------------------------------------"
+
+
 cd /opt/gatling
 ./bin/gatling.sh -nr -sf /opt/gatling-simulations -s simulations.BasicSimulation
 
