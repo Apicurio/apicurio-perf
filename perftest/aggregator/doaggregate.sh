d4ed050537f450196aa6b5c72bdce82bcba28b98
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-09 14:04:10 -0400

    ...

--- perftest/aggregator/doaggregate.sh
+++ perftest/aggregator/doaggregate.sh
@@ -2,13 +2,9 @@
 
 rm -rf /run/httpd/* /tmp/httpd*
 
-echo "--------- 1"
-/usr/sbin/apachectl -V
-echo "--------- 1"
-
-echo "--------- 2"
-cat /etc/httpd/conf/httpd.conf
-echo "--------- 2"
+echo "--------------"
+cat /etc/rsyncd.conf
+echo "--------------"
 
 echo ""
 echo ""
