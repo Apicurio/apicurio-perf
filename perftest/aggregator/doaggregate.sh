e79074ec410cbebb9161fe3fc8f1fd52ff5ed9f0
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-09 13:11:07 -0400

    ...

--- perftest/aggregator/doaggregate.sh
+++ perftest/aggregator/doaggregate.sh
@@ -2,8 +2,4 @@
 
 rm -rf /run/httpd/* /tmp/httpd*
 
-echo "---------------------"
-/usr/sbin/httpd --help
-echo "---------------------"
-
-echo "exec /usr/sbin/apachectl -DFOREGROUND"
+exec /usr/sbin/apachectl -DFOREGROUND -d /webroot -e debug
