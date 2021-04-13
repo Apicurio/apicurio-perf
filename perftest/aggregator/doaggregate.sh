d5fdb07ea1772665cb8d729cc1468023ad607058
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-13 10:02:54 -0400

    ...

--- perftest/aggregator/doaggregate.sh
+++ perftest/aggregator/doaggregate.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-echo "Configuring rsync-daemon"
-cp /tmp/rsyncd.conf /etc/rsyncd.conf
-mkdir -p /tmp/logs
-
-echo "Starting rsync-daemon"
-systemctl enable --now rsyncd 
-
 rm -rf /run/httpd/* /tmp/httpd*
 
-echo "-------------- cat rsyncd.conf"
-cat /etc/rsyncd.conf
+echo "-------------- cat /etc/vsftpd/vsftpd.conf"
+cat /etc/vsftpd/vsftpd.conf
 echo "--------------"
 echo ""
 echo ""
@@ -18,22 +11,9 @@ echo "-------------- java -version"
 java -version
 echo "--------------"
 
-#echo "Building and installed inotify-tools"
-#mkdir -p /tmp/work
-#cd /tmp/work
-#curl https://codeload.github.com/inotify-tools/inotify-tools/zip/refs/tags/3.20.11.0 -o /tmp/work/inotify-tools-3.20.11.0.zip
-#unzip inotify-tools-3.20.11.0.zip
-#cd /tmp/work/inotify-tools-3.20.11.0
-#./configure
-#make
-#make install
-
-echo ""
-echo ""
-echo "------------------------ inotify"
-ls -l /usr/local/bin/inotify*
-echo "------------------------"
-echo ""
+echo "Starting FTP server"
+vsftpd --help
+vsftpd --version
 
 #echo "Starting httpd"
 #/usr/sbin/apachectl -DFOREGROUND &
@@ -44,14 +24,23 @@ echo ""
 echo "Watching for simulation log files... (Ctrl-C to stop the aggregator)"
 WATCH_DIR=/tmp/logs
 LOGFILE=/tmp/watchlog.txt
-while : ; do
+#while : ; do
     ls -al /tmp/logs
     inotifywait $WATCH_DIR -t 5
     sleep 5
+
+    ls -al /tmp/logs
+    inotifywait $WATCH_DIR -t 5
+    sleep 5
+
+    ls -al /tmp/logs
+    inotifywait $WATCH_DIR -t 5
+    sleep 5
+
 #        inotifywait $watchdir | while read path action file; do
 #                ts=$(date +"%C%y%m%d%H%M%S")
 #                echo "$ts :: file: $file :: $action :: $path"
 #                echo "$ts :: file: $file :: $action :: $path">>$logfile
 #        done
-done
+#done
 echo "Done!"
