4eb93cdb50abbe6c7d78e6dd2b1944b804b78ad7
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-13 13:12:39 -0400

    ...

--- perftest/aggregator/doaggregate.sh
+++ perftest/aggregator/doaggregate.sh
@@ -6,6 +6,22 @@ echo "-------------- java -version"
 java -version
 echo "--------------"
 
+echo "Configuring sshd"
+
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+	# generate fresh rsa key
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+	# generate fresh dsa key
+	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
+#prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+  mkdir -p /var/run/sshd
+fi
+
 echo "Starting SSH server"
 whereis sshd
 /usr/sbin/sshd &
