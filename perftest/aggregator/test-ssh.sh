66880dcc2f4ef25c3837a7fb3abda04e3833bc2a
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-13 14:01:13 -0400

    ...

--- perftest/aggregator/test-ssh.sh
+++ perftest/aggregator/test-ssh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ssh -p 5022 -i id_rsa simuser@localhost
+#ssh -p 5022 -i id_rsa simuser@localhost
+sshpass -p "simpass" scp -P 5022 simuser@localhost:/home/simuser/simulation.log simulation.log
