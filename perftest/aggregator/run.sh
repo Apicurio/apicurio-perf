60462a432b2de3c2e2347b5f3d92c13216fb83a3
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-13 13:04:56 -0400

    ...

--- perftest/aggregator/run.sh
+++ perftest/aggregator/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -p 5080:80 -p 5021:21 -p 10000-10100:10000-10100 -it apicurio/apicurio-perftest-aggregator:latest
+docker run -p 5080:80 -p 5022:22 -it apicurio/apicurio-perftest-aggregator:latest
