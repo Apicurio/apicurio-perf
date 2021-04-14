11e0fde93508b6a8f1cbd771543203e43d32e4da
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-14 11:52:13 -0400

    ...

--- perftest/worker/run.sh
+++ perftest/worker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export REGISTRY_URL=http://127.0.0.1:9090/apis/registry/v1
-export TEST_RAMP_TIME=5
-export TEST_REPORT_RESULTS=true
-export TEST_AGGREGATOR_HOST=localhost
-export TEST_AGGREGATOR_PORT=5022
-
-docker run --network="host" -it apicurio/apicurio-perftest-worker:latest
+docker run --network="host" -it \
+    -e REGISTRY_URL=http://127.0.0.1:9090/apis/registry/v1 \
+    -e TEST_RAMP_TIME=5 \
+    -e TEST_REPORT_RESULTS=true \
+    -e TEST_AGGREGATOR_HOST=localhost \
+    -e TEST_AGGREGATOR_PORT=5022 \
+    apicurio/apicurio-perftest-worker:latest
