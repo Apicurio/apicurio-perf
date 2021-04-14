6ec727d33dcbdc1e0f312983cd6ddeeb8ce86ffe
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-14 12:12:10 -0400

    ...

--- perftest/aggregator/process.sh
+++ perftest/aggregator/process.sh
@@ -2,6 +2,19 @@
 
 LOGS_DIR=/home/simuser/logs
 HTML_LOGS_DIR=/var/www/html/logs
+RESULTS_DIR=$GATLING_HOME/results
 
 echo "Copying log files from $LOGS_DIR to $HTML_LOGS_DIR"
 cp -f $LOGS_DIR/*.log $HTML_LOGS_DIR
+
+echo "Copying log files from $LOGS_DIR to $RESULTS_DIR/aggregate"
+mkdir -p $RESULTS_DIR/aggregate
+cp -f $LOGS_DIR/*.log $RESULTS_DIR/aggregate
+
+echo "Generating aggregate simulation report..."
+cd /opt/gatling
+./bin/gatling.sh -ro aggregate
+
+find $GATLING_HOME -name '*.htm*'
+
+echo "Done!"
