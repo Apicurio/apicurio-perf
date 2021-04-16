6ede9ecce95464cc52f632b75b52c2a88a050b87
Eric Wittmann <eric.wittmann@gmail.com>
2021-04-16 13:51:04 -0400

    ...

--- perftest/aggregator/build.sh
+++ perftest/aggregator/build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+pushd .
+
+rm -rf target
+cd target
+git clone git@github.com:EricWittmann/apicurio-perftest-aggregator.git
+cd apicurio-perftest-aggregator
+mvn package -Pnative -Dquarkus.native.container-build=true
+
+popd
+cp target/apicurio-perftest-aggregator/target/apicurio*-runner target/aggregator
+
 docker build -t="apicurio/apicurio-perftest-aggregator" --rm .
